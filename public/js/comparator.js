031aace33d98b74629cec6293747a0f1bb0ab4cb
Suguru Motegi <hollow.03.03.bleach@gmail.com>
2014-12-25 21:35:10 +0900

    Update for async test

--- public/js/comparator.js
+++ public/js/comparator.js
@@ -1,4 +1,64 @@
 /* global performance */
+
+var util = {
+  reverse: reverse,
+  forEach: forEach,
+  times: times,
+  shuffle: shuffle
+};
+
+function reverse(func) {
+
+  return function(value, key) {
+    func(key, value);
+  };
+}
+
+function forEach(object, iterator) {
+
+  var i = -1;
+  var l = 0;
+  if (Array.isArray(object)) {
+    l = object.length;
+    while(++i < l) {
+      if (iterator(object[i], i, object) === false) {
+        break;
+      }
+    }
+  } else if (typeof object == 'object') {
+    var keys = Object.keys(object);
+    l = keys.length;
+    while(++i < l) {
+      var key = keys[i];
+      if (iterator(object[key], key, object) === false) {
+        break;
+      }
+    }
+  }
+  return object;
+}
+
+function times(n, iterator) {
+
+  var i = -1;
+  while(++i < n) {
+    iterator(i);
+  }
+}
+
+function shuffle(array) {
+
+  var index = -1;
+  var result = Array(array.length);
+  forEach(array, function(value) {
+    var rand = Math.floor(Math.random() * (++index + 1));
+    result[index] = result[rand];
+    result[rand] = value;
+  });
+  return result;
+}
+
+
 function Comparator() {
 
   this._funcs = {};
@@ -13,7 +73,7 @@ function Comparator() {
 Comparator.prototype.set = function set(name, func) {
 
   if (typeof name == 'object') {
-    _forEach(name, _reverse(set.bind(this)));
+    util.forEach(name, util.reverse(set.bind(this)));
   } else {
     this._funcs[name] = func;
   }
@@ -51,7 +111,7 @@ Comparator.prototype.get = function(name) {
 Comparator.prototype.option = function(options) {
 
   var self = this;
-  _forEach(options, function(value, key) {
+  util.forEach(options, function(value, key) {
     self._options[key] = value;
   });
   return this;
@@ -69,7 +129,7 @@ Comparator.prototype.start = function() {
 
   var times = self._options.times || 10;
   var results = {};
-  _forEach(keys, function(key) {
+  util.forEach(keys, function(key) {
     results[key] = Array(times);
   });
 
@@ -79,88 +139,82 @@ Comparator.prototype.start = function() {
     throw new Error('timer can not use');
   }
 
+  var count = 0;
+  var end = false;
   var gc = objectTypes[typeof global] && global && global.gc;
   if (self._options.async) {
-    self._start = true;
-    whilist();
+    self._started = true;
+    iterate();
     return self;
   }
-  _times(times, function(i) {
-    var sample = _shuffle(keys);
-    _forEach(sample, function(key) {
+  util.times(times, function(i) {
+    var sample = util.shuffle(keys);
+    util.forEach(sample, function(key) {
       var func = funcs[key];
       timer.init().start();
       func();
       var diff = timer.diff();
       results[key][i] = diff;
-      if (gc) {
-        gc();
-      }
     });
+    if (gc) {
+      gc();
+    }
   });
   self._results = results;
   return self;
 
-  function whilist() {
+  function iterate() {
 
-    var count = 0;
-    var end = false;
-    iterate();
-
-    function iterate() {
-
-      if (!end) {
-        iterator(function(err) {
-          if (err) {
-            return done(err);
-          }
-          if (count++ >= times) {
-            return done();
-          }
-          iterate();
-        });
-      } else {
-        done();
-      }
-    }
-
-    function done(err) {
-      self._start = false;
-      self._results = results;
-      self.emit('result', err);
-    }
-
-    function iterator(callback) {
-
-      var index = 0;
-      var sample = _shuffle(keys);
-      (function _iterate() {
-        var key = sample[index++];
-        if (!key) {
-          return callback();
+    if (!end) {
+      iterator(function(err) {
+        if (err) {
+          return done(err);
         }
-        timer.init().start();
-        funcs[key](function(err) {
-          var diff = timer.diff();
-          results[key][count] = diff;
-          if (err) {
-            return callback(err);
-          }
-          if (gc) {
-            gc();
-          }
-          _iterate();
-        });
-      })();
+        if (count++ >= times) {
+          return done();
+        }
+        iterate();
+      });
+    } else {
+      done();
     }
+  }
 
+  function done(err) {
+    self._started = false;
+    self._results = results;
+    self.emit('result', err);
+  }
+
+  function iterator(callback) {
+
+    var index = 0;
+    var sample = util.shuffle(keys);
+    (function _iterate() {
+      var key = sample[index++];
+      if (!key) {
+        return callback();
+      }
+      timer.init().start();
+      funcs[key](function(err) {
+        var diff = timer.diff();
+        results[key][count] = diff;
+        if (err) {
+          return callback(err);
+        }
+        if (gc) {
+          gc();
+        }
+        _iterate();
+      });
+    })();
   }
 };
 
 Comparator.prototype.on = function on(key, callback) {
 
   if (typeof key == 'object') {
-    _forEach(key, _reverse(on));
+    util.forEach(key, util.reverse(on));
     return this;
   }
   this._events[key] = this._events[key] || [];
@@ -171,7 +225,7 @@ Comparator.prototype.on = function on(key, callback) {
 Comparator.prototype.once = function once(key, callback) {
 
   if (typeof key == 'object') {
-    _forEach(key, _reverse(once));
+    util.forEach(key, util.reverse(once));
     return this;
   }
   callback._once = true;
@@ -187,27 +241,27 @@ Comparator.prototype.emit = function emit(key, err, res) {
     return this;
   }
   var deleteFlags = [];
-  _forEach(events, function(func, index) {
+  util.forEach(events, function(func, index) {
     func(err, res);
     if (func._once) {
       deleteFlags.unshift(index);
     }
   });
   if (deleteFlags.length) {
-    _forEach(deleteFlags, function(index) {
+    util.forEach(deleteFlags, function(index) {
       events.splice(index, 1);
     });
   }
   return this;
 };
 
-Comparator.prototype.result = function(callback) {
+Comparator.prototype.result = function result(callback) {
 
   var results = {};
   var opts = this._options;
   var async = opts.async;
-  if (async && this._start) {
-    this.once('result', this.result.bind(this, callback));
+  if (async && this._started) {
+    this.once('result', result.bind(this, callback));
     return 'calculating...';
   }
 
@@ -218,7 +272,7 @@ Comparator.prototype.result = function(callback) {
   var varia = opts.variance !== false;
   var dev = opts.standard_deviation !== false;
   var vs = !(opts.versus === false || opts.vs === false);
-  _forEach(this._results, function(array, key) {
+  util.forEach(this._results, function(array, key) {
     var l = array.length;
     if (!l) {
       return;
@@ -229,7 +283,7 @@ Comparator.prototype.result = function(callback) {
     }
     if (ave || varia || dev || vs) {
       var sum = 0;
-      _forEach(array, function(n) {
+      util.forEach(array, function(n) {
         sum += n;
       });
       var average = sum / array.length;
@@ -245,7 +299,7 @@ Comparator.prototype.result = function(callback) {
       if (varia || dev) {
         var variance = (function() {
           var variance = 0;
-          _forEach(array, function(value) {
+          util.forEach(array, function(value) {
             variance += Math.pow(value - average, 2);
           });
           return variance / l;
@@ -263,9 +317,9 @@ Comparator.prototype.result = function(callback) {
     results[key] = data;
   });
   if (vs) {
-    _forEach(results, function(data, key) {
+    util.forEach(results, function(data, key) {
       data.vs = {};
-      _forEach(results, function(_data, _key) {
+      util.forEach(results, function(_data, _key) {
         if (key === _key) {
           return;
         }
@@ -283,57 +337,6 @@ Comparator.prototype.result = function(callback) {
   return results;
 };
 
-function _reverse(func) {
-
-  return function(value, key) {
-    func(key, value);
-  };
-}
-
-function _forEach(object, iterator) {
-
-  var i = -1;
-  var l = 0;
-  if (Array.isArray(object)) {
-    l = object.length;
-    while(++i < l) {
-      if (iterator(object[i], i, object) === false) {
-        break;
-      }
-    }
-  } else if (typeof object == 'object') {
-    var keys = Object.keys(object);
-    l = keys.length;
-    while(++i < l) {
-      var key = keys[i];
-      if (iterator(object[key], key, object) === false) {
-        break;
-      }
-    }
-  }
-  return object;
-}
-
-function _times(n, iterator) {
-
-  var i = -1;
-  while(++i < n) {
-    iterator(i);
-  }
-}
-
-function _shuffle(array) {
-
-  var index = -1;
-  var result = Array(array.length);
-  _forEach(array, function(value) {
-    var rand = Math.floor(Math.random() * (++index + 1));
-    result[index] = result[rand];
-    result[rand] = value;
-  });
-  return result;
-}
-
 function getTimer() {
 
   if (objectTypes[typeof process] && process && process.hrtime) {
@@ -343,6 +346,7 @@ function getTimer() {
     return performanceTimer();
   }
 }
+Comparator.prototype.getTimer = getTimer;
 
 function nodeTimer() {
 
